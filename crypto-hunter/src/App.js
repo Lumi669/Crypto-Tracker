bef1c4cb5ae7040f492d2405170b2e3a8a648c5f
cmjiwa <wangjinghuan@yahoo.com>
2022-07-25 15:02:09 +0300

    Add BrowserRouter and Header.js

--- crypto-hunter/src/App.js
+++ crypto-hunter/src/App.js
@@ -1,7 +1,15 @@
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
+import Header from "./Componentts/Header";
 
 function App() {
-  return <div className="App">fgdfgggggggggffff</div>;
+  return (
+    <BrowserRouter>
+      <div>
+        <Header />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
