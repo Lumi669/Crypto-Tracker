f5f8725fbeba207fba1f0354f334b448c6876294
cmjiwa <wangjinghuan@yahoo.com>
2022-07-25 17:28:26 +0300

    Add several pages

--- crypto-hunter/src/App.js
+++ crypto-hunter/src/App.js
@@ -1,12 +1,18 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./Componentts/Header";
+import Homepage from "./Pages/Homepage";
+import CoinPage from "./Pages/CoinPage";
 
 function App() {
   return (
     <BrowserRouter>
       <div>
         <Header />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/coins/:id" element={<CoinPage />} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
